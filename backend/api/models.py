fe980c77807b195bab76f45e03446db8bca77b1a
rustic-d3 <dumbravarustic454@gmail.com>
2025-07-28 14:48:14 +0300

    added the role models and custom user model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('teacher', 'Teacher'),
+        ('children', 'Children')]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='notes')
+
+    def __str__(self):
+        return self.title
+    
+    
+
+class Teacher(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Child(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    personal_id = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class ClassRoom(models.Model):
+    title = models.CharField(max_length=100)
+    type = models.CharField(max_length=50)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='classes')
+    children = models.ManyToManyField(Child, related_name='classes')
+    start_date = models.DateField()
+    end_date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return self.title
+
+
+class Admin:
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 
-from django.contrib.auth.models import User
 from rest_framework import generics
+from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
+from .models import Note
 
-
+User = get_user_model()
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]  # Allow any user to create an account
+
+
+class CreateNoteView(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author = user)
