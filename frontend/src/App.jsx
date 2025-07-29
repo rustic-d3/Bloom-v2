7f1b3325a29978393131b92a72cac24e3be44e32
rustic-d3 <dumbravarustic454@gmail.com>
2025-07-30 00:14:41 +0300

    created the simple auth system t front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,52 @@
+import React from "react";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+import NotFound from "./components/NotFound";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import api from "./api";
 
-import React from 'react';
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+}
+
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Navigate to="/register" />;
+}
+
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="*" element={<NotFound />} />
+
+          
+
+          
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
