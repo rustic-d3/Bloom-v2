9a66060b6e62c8813e4ff922bdef3ef7bfe350e8
rustic-d3 <dumbravarustic454@gmail.com>
2025-09-05 15:48:26 +0300

    Preparing the frontend for developement

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { ACCESS_TOKEN } from './constants';
+const BASE_URL = import.meta.env.MODE == 'development' ? import.meta.env.VITE_API_URL_LOCAL : import.meta.env.VITE_API_URL_DEPLOY
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL 
+    baseURL: BASE_URL 
 })
 
 
